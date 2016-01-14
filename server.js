8bf9395395a83ecf9a7b423e058ec7bbf24e8395
Jon Chiu <jonathanchiu7@gmail.com>
2016-01-14 16:11:31 -0500

    Add route for querying JSON data containing sample users

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ var express = require('express');
 var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 app.get('/hello', function(req, res){
   res.send('hello world');
 });
+
+app.get('/user', function(req, res) {
+  var users = [
+    {username: "alice", firstName: "Alice", lastName: "Wonderland"},
+    {username: "jon", firstName: "Jonny", lastName: "Chiu"},
+  ];
+
+  res.json(users);
+});
+
 app.listen(port, ipaddress);
