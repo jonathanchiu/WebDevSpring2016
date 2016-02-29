14c836949fc9c6999b4dd647f0ca0d2185cad6a7
Jon Chiu <jonathanchiu7@gmail.com>
2016-02-29 14:28:35 -0500

    Get rid of http prefix in API calls to fix mixed content browser error

--- public/project/services/movie.service.client.js
+++ public/project/services/movie.service.client.js
@@ -8,7 +8,7 @@
   function MovieService($http) {
 
     function searchMovie(searchedMovie, page) {
-      var url = "http://www.omdbapi.com/?s=MOVIE&page=PAGE&type=movie";
+      var url = "//www.omdbapi.com/?s=MOVIE&page=PAGE&type=movie";
       url = url.replace("MOVIE", searchedMovie);
       url = url.replace("PAGE", page.toString());
 
@@ -19,7 +19,7 @@
     }
 
     function getMovieDetails(id) {
-      var url = "http://www.omdbapi.com/?i=IMDB_ID&plot=full&r=json";
+      var url = "//www.omdbapi.com/?i=IMDB_ID&plot=full&r=json";
       url = url.replace("IMDB_ID", id);
 
       return $http.get(url)
